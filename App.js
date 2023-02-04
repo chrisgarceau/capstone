d208e1ad1f5a230fcb5551c390fa437b2072dd0d
chrisgarceau <cgarceau7399@gmail.com>
2023-02-04 18:52:29 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { MyTabs } from './Tabs.js'
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
+
