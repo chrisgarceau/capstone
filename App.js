ed7dfbb17a4ae81d8b928d297a314f350fbe61f4
chrisgarceau <cgarceau7399@gmail.com>
2023-04-08 21:44:25 -0400

    modified:   App.js 	deleted:    frontendPics/MLpage.png 	deleted:    frontendPics/homePage.png 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { MyTabs } from './Tabs.js'
+import {useState, useEffect} from "react"
+import AnimatedSplash from "react-native-animated-splash-screen";
 
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 2000);
+  
   return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
+    <AnimatedSplash
+      translucent={true}
+      isLoaded={loading}
+      logoImage={require("./assets/mcsp.png")}
+      backgroundColor={"#228647"}
+      logoHeight={400}
+      logoWidth={400}
+    >
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </AnimatedSplash>
+    
   );
 }
